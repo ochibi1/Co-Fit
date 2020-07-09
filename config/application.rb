5eed24151624a505a097afd9cc45c224c18a299d
ochibi1 <lapotit0122@gmail.com>
2020-07-09 22:59:59 +0900

    config generators

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,11 @@ module CoFit
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
