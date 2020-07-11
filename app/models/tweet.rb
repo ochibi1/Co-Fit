293727890ce41c922b19ac230289787a125ca321
ochibi1 <lapotit0122@gmail.com>
2020-07-11 22:10:23 +0900

    model association

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,7 @@
 class Tweet < ApplicationRecord
-  validates :text, presence: true
+  belongs_to :user
+  has_many :comments
+  has_many :tags, through: :tweets_tags
+
+  validates :message, presence: true
 end
